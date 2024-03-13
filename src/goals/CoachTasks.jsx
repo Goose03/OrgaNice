92011831b482e3affaa769282e5b26a9374cb715
Goose <gusgt03@gmail.com>
2024-03-12 19:32:04 -0600

    Gemini Setup

--- src/goals/CoachTasks.jsx
+++ src/goals/CoachTasks.jsx
@@ -1,7 +1,33 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
 export default function CoachTasks() {
-    return (
+  
+  const googleApiKey = "AIzaSyBXEn_QSHT40vlGfPoTpQhadfE44Vk-NkU";
+  try{
+    const genAI = new GoogleGenerativeAI(googleApiKey);
+
+    async function run() {
+      // For text-only input, use the gemini-pro model
+      const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    
+      const prompt = "Give me a three month long plan to start learning how to play the guitar with weekly objectives, each weak should have 7 different activities and should be made in list form separating each week by three = signs."
+    
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+    }
+    
+    run();
+  }
+  catch(error){
+    console.log(error)
+  }
+  
+
+  return (
       <>
-        <div className="bg-red-50 text-2xl">
+        <div className="bg-red-900 text-2xl w-1/2 rounded-lg h-64">
           <p>Hello World!</p>
         </div>
       </>
