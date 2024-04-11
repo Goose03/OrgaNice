d57f500485df587450e56dff6ef6a3ac83d13ea4
Jesús Enrique Bañales López <A01642425@tec.mx>
2024-04-11 14:15:20 -0600

    Login sin boton
    ya esta el login, falta configurarlo para que no se vea

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Login from "./login/Login"
 export default function App() {
 
   return (
     <>
         <div className='bg-bwhite h-screen text-nanum-gothic-coding'>
-            
+            <Login />
         </div>
     </>
   )
