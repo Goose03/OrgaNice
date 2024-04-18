c94a3e4e257a3f9493972192a270786848b890f5
Goose <gusgt03@gmail.com>
2024-04-17 18:43:48 -0600

    goals start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import CoachTasks from './goals/CoachTasks'
 
+
 export default function App() {
 
   return (
     <>
         <div className='bg-bwhite h-screen text-nanum-gothic-coding'>
-            
+          <CoachTasks />
         </div>
     </>
   )
--- src/goals/CoachTasks.jsx
+++ src/goals/CoachTasks.jsx
@@ -1,8 +1,14 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function CoachTasks() {
   
-  const googleApiKey = "AIzaSyBXEn_QSHT40vlGfPoTpQhadfE44Vk-NkU";
+  /*const googleApiKey = "AIzaSyBXEn_QSHT40vlGfPoTpQhadfE44Vk-NkU";
   try{
     const genAI = new GoogleGenerativeAI(googleApiKey);
 
@@ -22,13 +28,45 @@ export default function CoachTasks() {
   }
   catch(error){
     console.log(error)
-  }
+  }*/
   
 
   return (
       <>
-        <div className="bg-red-900 text-2xl w-1/2 rounded-lg h-64">
-          <p>Hello World!</p>
+        <div className="bg-mpurp text-twhite m-5 p-5 w-3/4 opacity-75 border-r rounded-2xl">
+          <p className="text-xl">Metas Personales</p>
+
+          <div className="flex">
+            <div className="bg-dpurp w-1/2 h-full flex">
+              <div className="bg-tred w-3/4">Matriz</div>
+              <div className="w-1/4">Boton</div>
+            </div>
+
+            <div className="bg-twhite w-1/2">
+              <CircularProgressbarWithChildren
+                value={75}
+                strokeWidth={8}
+                styles={buildStyles({
+                  pathColor: "#250E47",
+                  trailColor: "transparent"
+                })}
+              >
+                {/*
+                  Width here needs to be (100 - 2 * strokeWidth)% 
+                  in order to fit exactly inside the outer progressbar.
+                */}
+                <div style={{ width: "80%" }}>
+                  <CircularProgressbar
+                    value={70}
+                    styles={buildStyles({
+                      pathColor: "#250E47",
+                      trailColor: "transparent"
+                    })}
+                  />
+                </div>
+              </CircularProgressbarWithChildren>
+            </div>
+          </div>
         </div>
       </>
       
