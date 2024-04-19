788b822ee76cce03fa6ea4dbf3a3a0939b3e3511
Goose <gusgt03@gmail.com>
2024-04-19 15:01:00 -0600

    progress size fix

--- src/goals/CoachTasks.jsx
+++ src/goals/CoachTasks.jsx
@@ -1,4 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+
 import {
   CircularProgressbar,
   CircularProgressbarWithChildren,
@@ -6,9 +7,16 @@ import {
 } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
+import { useState } from "react";
+
 export default function CoachTasks() {
+  const dummyDaily = "10101110010111010011011010011101001110010101101100010111101011001001010100100t0000000000000000000000"
+  const dummyGoal =  {total: 90, week: 25, progress: dummyDaily};
+
+  const [goal, setGoal] = useState(dummyGoal);
   
-  /*const googleApiKey = "AIzaSyBXEn_QSHT40vlGfPoTpQhadfE44Vk-NkU";
+  /*
+  const googleApiKey = "AIzaSyBXEn_QSHT40vlGfPoTpQhadfE44Vk-NkU";
   try{
     const genAI = new GoogleGenerativeAI(googleApiKey);
 
@@ -28,41 +36,50 @@ export default function CoachTasks() {
   }
   catch(error){
     console.log(error)
-  }*/
+  }
+  */
   
 
   return (
       <>
-        <div className="bg-mpurp text-twhite m-5 p-5 w-3/4 opacity-75 border-r rounded-2xl">
+        <div className="bg-mpurp text-twhite m-5 p-5 w-1/2 opacity-75 border-r rounded-2xl">
           <p className="text-xl">Metas Personales</p>
 
-          <div className="flex">
+          <div className="flex justify-between ">
             <div className="bg-dpurp w-1/2 h-full flex">
-              <div className="bg-tred w-3/4">Matriz</div>
+              <div className="bg-tred w-3/4">
+                Matriz
+              </div>
               <div className="w-1/4">Boton</div>
             </div>
-
-            <div className="bg-twhite w-1/2">
+            
+            <div  className=" h-1/2">
               <CircularProgressbarWithChildren
-                value={75}
+                value={goal.total}
                 strokeWidth={8}
                 styles={buildStyles({
                   pathColor: "#250E47",
-                  trailColor: "transparent"
+                  trailColor: "transparent",
                 })}
+                className="grow-0"
               >
-                {/*
-                  Width here needs to be (100 - 2 * strokeWidth)% 
-                  in order to fit exactly inside the outer progressbar.
-                */}
-                <div style={{ width: "80%" }}>
-                  <CircularProgressbar
-                    value={70}
+                
+                <div style={{ width: "80%"}}>
+                  <CircularProgressbarWithChildren
+                    value={goal.week}
                     styles={buildStyles({
                       pathColor: "#250E47",
                       trailColor: "transparent"
                     })}
-                  />
+                    className=""
+                  >
+
+                    <div className="text-dpurp text-2xl font-bold">
+                      <p>{goal.total}%</p>
+                      <p>{goal.week}%</p>
+                    </div>
+
+                  </CircularProgressbarWithChildren>
                 </div>
               </CircularProgressbarWithChildren>
             </div>
