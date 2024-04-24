3aec63a5dd5efdd5d3c1baa4b2ec49f06ce15db9
Jesús Enrique Bañales López <A01642425@tec.mx>
2024-04-23 19:24:32 -0600

    fini
    a

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,63 +1,94 @@
-import './estilo.css'
-export default function Login(){
-    return(
-        <>
-    <body>
-        <header>
-            <nav class = "navegacion">
-                <ul class="menu">
-                    <li><a href="#">Opciones</a></li>
-                    <li><a href="#">Cuenta</a>
-                        <ul class="submenu">
-                            <li><a href="#">Sign in</a></li>
-                            <li><a href="#">Log in</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-<section class="bg-gray-50 dark:bg-gray-900">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-          Flowbite    
-      </a>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign in to your account
-              </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                  </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div class="flex items-center justify-between">
-                      <div class="flex items-start">
-                          <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                          </div>
-                          <div class="ml-3 text-sm">
-                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                          </div>
-                      </div>
-                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                  </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                  </p>
-              </form>
-          </div>
-      </div>
-  </div>
-</section>
-</body>
+import './estilo.css';
+import Popup from 'reactjs-popup';
 
+export default function Login() {
+    const captura = () => {
+        // Obtener los valores de los campos de entrada
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
+    return (
+        <>
+            <header>
+                <nav className="navegacion">
+                    <ul className="menu">
+                        <li><a href="#">Opciones</a></li>
+                        <li><a href="#">Cuenta</a>
+                            <ul className="submenu">
+                            <Popup trigger={<li><a href="#">Log in</a></li>} modal nested>
+                                    {close => (
+                                        <div className="modal">
+                                            <section className="bg-gray-50 dark:bg-gray-900">
+                                                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                                                    <div className="w-full bg-tpurp rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                                                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                                            <h1 className="text-xl font-bold leading-tight tracking-tight text-bwhite md:text-2xl dark:text-white">
+                                                                Log into your account
+                                                            </h1>
+                                                            <form className="space-y-4 md:space-y-6" action="#">
+                                                                <div>
+                                                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-bwhite dark:text-white">Your username</label>
+                                                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required=""/>
+                                                                </div>
+                                                                <div>
+                                                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-bwhite dark:text-white">Password</label>
+                                                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                                                                </div>
+                                                                <div className="flex items-center justify-between">
+                                                                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-dpurp">Forgot password?</a>
+                                                                </div>
+                                                                <button type="button" className="w-full text-bwhite bg-mpurp hover:bg-dpurp focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={captura}>Log in</button>
+                                                                <p className="text-sm font-light text-gray-500 dark:text-bwhite">
+                                                                    Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-dpurp">Sign up</a>
+                                                                </p>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </section>
+                                        </div>
+                                    )}
+                                </Popup>
+
+                                <Popup trigger={<li><a href="#">Sign in</a></li>} modal nested>
+                                    {close => (
+                                        <div className="modal">
+                                            <section className="bg-gray-50 dark:bg-gray-900">
+                                                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                                                    <div className="w-full bg-tpurp rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                                                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                                            <h1 className="text-xl font-bold leading-tight tracking-tight text-bwhite md:text-2xl dark:text-white">
+                                                                Sign in your account
+                                                            </h1>
+                                                            <form className="space-y-4 md:space-y-6" action="#">
+                                                                <div>
+                                                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-bwhite dark:text-white">Your username</label>
+                                                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required=""/>
+                                                                </div>
+                                                                <div>
+                                                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-bwhite dark:text-white">Password</label>
+                                                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                                                                </div>
+                                                                <button type="button" className="w-full text-bwhite bg-mpurp hover:bg-dpurp focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={captura}>Sign in</button>
+                                                                <p className="text-sm font-light text-gray-500 dark:text-bwhite">
+                                                                </p>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </section>
+                                        </div>
+                                    )}
+                                </Popup>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </>
     );
 }
