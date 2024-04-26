0be46d83b4a44692a3545f64f883b40b561e4fb2
LuzPaty02 <luzpatyhr@gmail.com>
2024-04-26 10:55:13 -0600

    servidor activo Hello World

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-const express = required("express");
-const mysql= require("mysql");
-const cors=require("cors");
-
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
 
 
 const Port = 5000;
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 app.listen(Port, () => {
     console.log(`Servido ${Port}`);
 })
