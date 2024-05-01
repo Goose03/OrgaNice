fda5d4a12ce2c9ea989f91a36196830cffbc8251
goose <gusgt03@gmail.com>
2024-04-30 21:27:35 -0600

    tareas

--- src/pages/teacher/tareas/ClaseInfo.jsx
+++ src/pages/teacher/tareas/ClaseInfo.jsx
@@ -1,18 +1,54 @@
-import { useParams } from "react-router"
+import { useParams } from "react-router";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { useState } from "react";
+import Popup from 'reactjs-popup';
 
-import TareaDesc from "./TareaDesc"
+import TareaDesc from "./TareaDesc";
 
 export default function ClaseInfo(props){
     const { id } = useParams();
 
+    const dummyClase = { nom: "Matematicas 2", cantAlu: 32, tarAct: 6,  };
+
+    const dummyTareas = [{id:123, nombre:'Practica 1', priori:2, fecha:'00/00/0000', cantEntre: 12},
+                         {id:124, nombre:'Practica 2', priori:3, fecha:'00/00/0000', cantEntre: 2}
+    ]   
+
+    const [clase, setClase] = useState(dummyClase);
+    const [tareas, setTareas] = useState(dummyTareas);
+
+    const [clipboard, setClipboard] = useState('Copy to Clipboard')
+
+    const arrowStyle = { color: '#250E47' }; 
+    
     return(
         <>
-            <div className="bg-mpurp h-full">
+            <div className="bg-mpurp h-screen">
                 <div className="text-twhite p-5">
-                    <h1 className="text-6xl">Clase {id}</h1>
+                    <h1 className="text-6xl">{clase.nom}</h1>
+
+                    <p className="my-5 text-lg">{clase.cantAlu} Alumnos | {clase.tarAct} Tareas Activas</p>
+
+                    <div className="border-2 border-dpurp rounded-xl pl-2 flex text-2xl w-fit justify-center items-center">
+                        <p className="pr-2 border-r-2 border-dpurp">Codigo de Inscripcion</p>
+                        <CopyToClipboard 
+                            text={id}
+                            >
+                            <Popup
+                                trigger={<button className="hover:bg-dpurp/[.5] p-2" onClick={() => setClipboard('Copeid!')}>{id}</button>}
+                                position={'top center'}
+                                on={['hover', 'focus']}
+                                {...{arrowStyle}}
+                                >
+                                    <p className="bg-dpurp/80 text-twhite p-2 rounded-lg">{clipboard}</p>
+                            </Popup>
+                        </CopyToClipboard>
+                    </div>
                     
                     <div className="divider"></div>
-                    <TareaDesc />
+                    {tareas.map((tarea) => {
+                        return <TareaDesc key={tarea.id} tarea={tarea} cantAlu={clase.cantAlu} />;
+                    })}
                 </div>
             </div>
         </>
--- src/pages/teacher/tareas/TareaDesc.jsx
+++ src/pages/teacher/tareas/TareaDesc.jsx
@@ -1,10 +1,40 @@
+import Popup from 'reactjs-popup';
+import 'ldrs/ripples'
 
 export default function TareaDesc(props){
 
     return(
         <>
-            <div className="bg-bwhite/[.60]">
-                hi
+            <div className="flex justify-between bg-bwhite/[.60] p-5 m-5 rounded-lg text-dpurp">
+                <div className="">
+                    <h1>{props.tarea.nombre}</h1>
+                    <p>{props.tarea.fecha}</p>
+                    <p>Proiroidad:{props.tarea.priori}</p>
+                    <p>Respuestas: {props.tarea.cantEntre}</p>
+                </div>
+
+                <div className={`border-2 border-dpurp ${props.tarea.cantEntre / props.cantAlu > 0.2 ? 'hover:bg-dpurp/30' : ''} flex justify-center p-2 rounded-lg`}>
+                    <Popup
+                        trigger={
+                            <button disabled={((props.tarea.cantEntre / props.cantAlu) > 0.2) ? false : true } >
+                                <p>Obtener Retro</p>
+                                <p>{Math.floor(props.tarea.cantEntre / props.cantAlu * 100)}%</p>
+                            </button>
+                        }
+                        modal
+                    >
+                        <div className='bg-mpurp border-2 border-dpurp/50 text-twhite p-5 flex flex-col justify-center items-center rounded-lg'>
+                            <p>Creando Retro</p>
+                            <div className='p-2 w-fit'>
+                                <l-ripples
+                                size="45"
+                                speed="2"
+                                color="white"
+                                ></l-ripples>
+                            </div>
+                        </div>
+                    </Popup>
+                </div>
             </div>
         </>
     )
